c82da4929e5eb352ca95eb9dc694adeb6d2ca8ff
Adam Tse <mr.adam.tse@gmail.com>
2017-01-10 17:39:05 -0500

    display picture

--- src/pages/play/play.ts
+++ src/pages/play/play.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { ToastController, NavController } from 'ionic-angular';
 
 import * as PIXI from 'pixi.js';
@@ -13,9 +13,8 @@ clicks: number = 0;
 title: String = "Game Number";
 
 
-  constructor(public toastCtrl: ToastController, public navCtrl: NavController, public canvas: ElementRef){
+  constructor(public toastCtrl: ToastController, public navCtrl: NavController ){
     this.presentToast();
-    this.canvas;
   }
 
 
@@ -40,7 +39,34 @@ title: String = "Game Number";
     // let renderer = PIXI.autoDetectRenderer(400, 100,{backgroundColor : 0x000000});
     let renderer = new PIXI.WebGLRenderer(400, 100);
     let view = document.getElementById('canvas');
+    let stage = new PIXI.Container();
+
+    renderer.autoResize = true;
+    renderer.resize(window.innerWidth - 60, 100);
     view.appendChild(renderer.view);
+    renderer.render(stage);
+
+
+    PIXI.loader
+  .add("assets/basics/bunny.png")
+  .load(setup);
+
+  //This `setup` function will run when the image has loaded
+  function setup() {
+
+    //Create the `cat` sprite from the texture
+    var cat = new PIXI.Sprite(
+      PIXI.loader.resources["assets/basics/bunny.png"].texture
+    );
+
+    //Add the cat to the stage
+    stage.addChild(cat);
+
+    //Render the stage
+    renderer.render(stage);
+  }
+
+
   }
 
 
